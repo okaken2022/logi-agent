94d9afba366554e0702b836de62ecb39cf559d8d
okaken2022 <okaken.address@gmail.com>
2022-07-26 00:03:39 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".swiper", {
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+  loop: true,
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: true
+	},
+  effect: "fade"
+});
